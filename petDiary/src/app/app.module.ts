5ce512b7a8991e614127fdca1aae641db0a5480f
bonomiandreia <bonomiandreia@gmail.com>
2021-07-23 15:03:36 -0300

    created components and pages

--- petDiary/src/app/app.module.ts
+++ petDiary/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsModule } from './pages/posts/posts.module';
+import { LoginModule } from './pages/login/login.module';
+import { CreateAccountModule } from './pages/create-account/create-account.module';
+import { HeaderComponent } from './components/header/header.component';
+import { PostsTableComponent } from './components/posts-table/posts-table.component';
+import { ModalPostComponent } from './components/modal-post/modal-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostsTableComponent,
+    ModalPostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PostsModule,
+    LoginModule,
+    CreateAccountModule
   ],
   providers: [],
   bootstrap: [AppComponent]
