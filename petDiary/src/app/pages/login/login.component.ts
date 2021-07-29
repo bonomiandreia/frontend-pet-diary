70bb2e72625b5e3443fd513e2fc525512afcf733
bonomiandreia <bonomiandreia@gmail.com>
2021-07-29 19:53:56 -0300

    reactive form

--- petDiary/src/app/pages/login/login.component.ts
+++ petDiary/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  login(): void {
+    //
   }
 
 }
