91b7e0c6b1451984ab1c2a96e4f049a6b6d67c35
bonomiandreia <bonomiandreia@gmail.com>
2021-07-29 23:44:06 -0300

    interceptor

--- petDiary/src/app/interceptor/token-interceptor.ts
+++ petDiary/src/app/interceptor/token-interceptor.ts
@@ -11,28 +11,28 @@ import { LoginQuery } from '../state/login/login.query';
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
   token$: Observable<string>;
+  token: string
 
   constructor(public query: LoginQuery) {
     this.token$ = this.query.token$;
+    this.token = this.query.getValue().token;
   }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const ignoreInterceptor = request?.body?.ignore_interceptor ? request?.body?.ignore_interceptor : false;
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const ignoreInterceptor = request?.body?.ignore_interceptor ? request?.body?.ignore_interceptor : false;
+        
+        if ((request?.body && ignoreInterceptor) || request?.url.includes('.svg')) {
+            return next.handle(request);
+        }
 
-    if ((request?.body && ignoreInterceptor) || request?.url.includes('.svg')) {
-        return next.handle(request);
+        const tokenizedReq = request.clone({
+            setHeaders: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            auth: `${this.token}`
+            }
+        });
+    
+        return next.handle(tokenizedReq);
     }
-
-    return this.token$.pipe(
-      switchMap(token => {
-          let headers = request.headers;
-          headers = headers.set('auth', `${token}`);
-          const requestCopy = request.clone({
-              headers,
-          });
-
-          return next.handle(requestCopy);
-      })
-    );
-  }
 }
