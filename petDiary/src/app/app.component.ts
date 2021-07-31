a64271fd305936853b5dc8fd623473e4764b74d4
bonomiandreia <bonomiandreia@gmail.com>
2021-07-31 18:28:17 -0300

    layout app

--- petDiary/src/app/app.component.ts
+++ petDiary/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'petDiary';
+  isMobile: boolean;
+
+  @HostListener('window:resize', ['$event'])
+
+  onResize(event) {
+    event?.target?.innerWidth < 768 ? this.isMobile = true : this.isMobile = false;
+    console.log(this.isMobile)
+  }
 }
