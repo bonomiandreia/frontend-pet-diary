553f57f91613f5925a21fed308de4e27694eb057
bonomiandreia <bonomiandreia@gmail.com>
2021-08-03 18:31:57 -0300

    const mocks

---
+++ petDiary/src/app/mocks/login/login.mock.ts
@@ -0,0 +1,7 @@
+export const loginMock = {
+    token: 'token',
+    auth: {
+        _id: '123',
+        email: 'andreia.bonomi@bonomi.com.br'
+    }
+}
---
+++ petDiary/src/app/mocks/login/post.mock.ts
@@ -0,0 +1,19 @@
+export const postMock = 
+ [
+    {
+        date: "10/12/2022",
+        idUser: "6042eaee65482059cb025975",
+        text: "Lorem ipsum dolor sit amet",
+        __v: 0,
+        _id: "61004b633e07d5d89fac20c4",
+
+    },
+    {
+        date: "10/12/2022",
+        idUser: "6042eaee65482059cb025975",
+        text: "Lorem ipsum dolor sit amet",
+        __v: 0,
+        _id: "61004b633e07d5d89fac20c4",
+
+    },
+]
