606f0b282edc90aac13798dd396fe78a9638fccf
bonomiandreia <bonomiandreia@gmail.com>
2021-08-03 18:59:35 -0300

    domSanitizer

--- petDiary/src/app/pipes/pipe-module.pipe.spec.ts
+++ petDiary/src/app/pipes/pipe-module.pipe.spec.ts
@@ -1,8 +1,19 @@
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { PipeModulePipe } from './pipe-module.pipe';
+import { TestBed } from '@angular/core/testing';
 
-describe('PipeModulePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PipeModulePipe();
+describe('Pipe: SafeResourceUrl', () => {
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BrowserModule],
+    })
+});
+
+  it('should create an instance', () => {
+    const domSanitizer = TestBed.get(DomSanitizer);
+    
+    const pipe = new PipeModulePipe(domSanitizer);
     expect(pipe).toBeTruthy();
   });
 });
