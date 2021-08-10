ece330ec2d32bf4383e135e3ae3b19a706ef4c1d
bonomiandreia <bonomiandreia@gmail.com>
2021-08-10 19:37:01 -0300

    init change modes

--- petDiary/src/app/components/header/header.component.ts
+++ petDiary/src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { resetStores } from '@datorama/akita';
 import { Router } from '@angular/router';
 import { LoginQuery } from '../../state/login/login.query';
+import { LayoutService } from '../../state/layout/layout.service';
 
 @Component({
   selector: 'app-header',
@@ -13,8 +14,9 @@ export class HeaderComponent implements OnInit {
   email: string;
   eventMode: boolean;
 
-  constructor(private query: LoginQuery, private router: Router) { 
+  constructor(private query: LoginQuery, private router: Router, private serviceLayout: LayoutService) { 
     this.eventMode = false;
+    this.serviceLayout.initTheme();
   }
 
   ngOnInit(): void {
